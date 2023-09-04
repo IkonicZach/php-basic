6092e2c5b5bf97979428df3262efa1fd8f34ed3a
Minn Thu Ta <minnthuta606@gmail.com>
2023-09-04 12:17:54 +0630

    second day learning

---
+++ 21 to 40/hereDoc.php
@@ -0,0 +1,10 @@
+<?php
+$doc = <<<start
+    <h1> Welcome </h1>
+    <p> to the best shopping website in the world</p>
+    <b> We offer you the best items with most resonable values.<br></b>
+    <i> Come shop with us <b>right now.</b></i>
+start;
+
+echo $doc;
+?>
